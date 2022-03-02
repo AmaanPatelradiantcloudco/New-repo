14c0b1f5666fda3248e459ad2878dc4bd115688b
AmaanPatel <94842098+AmaanPatelradiantcloudco@users.noreply.github.com>
2022-03-02 13:18:10 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>
+        Start editing to see some magic happen!Well this turns out to be a super
+        interactive page
+      </h2>
+    </div>
+  );
+}
